3696594f82985136eaf0ac04bdd4803c0efb5dda
ank010 <ank010@ucsd.edu>
2022-01-20 11:26:15 -0800

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -11,14 +11,15 @@ public class MarkdownParse {
         // the next )
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
-            System.out.println(currentIndex);
             int nextOpenBracket = markdown.indexOf("[", currentIndex);
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
             toReturn.add(markdown.substring(openParen + 1, closeParen));
+            if(nextOpenBracket != 0 && markdown.charAt(nextOpenBracket-1) != '!'){
+                toReturn.add(markdown.substring(openParen + 1, closeParen)); 
+            }
             currentIndex = closeParen + 1;
-            System.out.println(currentIndex);
         }
         return toReturn;
     }
