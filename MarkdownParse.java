1d74c9cf967e4c8a9f8d36ad806df767cbae4788
ank010 <ank010@ucsd.edu>
2022-02-03 10:54:44 -0800

    Fix for Test5

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -16,10 +16,12 @@ public class MarkdownParse {
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
             if(openParen != -1 && closeParen != -1){
-                String substring = markdown.substring(openParen+1, closeParen);
-                if(!substring.contains(" ")){
-                    if(nextOpenBracket != 0 && markdown.charAt(nextOpenBracket-1) != '!'){
-                        toReturn.add(markdown.substring(openParen + 1, closeParen)); 
+                if(openParen - nextCloseBracket == 1){
+                    String substring = markdown.substring(openParen+1, closeParen);
+                    if(!substring.contains(" ")){
+                        if(nextOpenBracket != 0 && markdown.charAt(nextOpenBracket-1) != '!'){
+                             toReturn.add(markdown.substring(openParen + 1, closeParen)); 
+                        }
                     }
                 }
             }
